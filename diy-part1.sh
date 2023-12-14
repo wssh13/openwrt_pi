fe567ec8d2ddac6b1e71b3af634f803ee96880b5
刘荣富 <42900477+wssh13@users.noreply.github.com>
2023-12-14 22:05:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git  kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-#git clone https://github.com/gngpp/luci-theme-design.git  >>package/luci-theme-design
+# echo 'src-git  kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+# echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+# git clone https://github.com/gngpp/luci-theme-design.git  >>package/luci-theme-design
